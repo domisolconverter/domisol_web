f684db8183f01b7ed5abc4c4fb5ac02dc0134fb1
wkdthf21 <wkdthf21@naver.com>
2019-06-02 22:29:38 +0900

    change main css

--- main/converterThread.py
+++ main/converterThread.py
@@ -22,9 +22,11 @@ class ConverterThread(threading.Thread):
         # (수정) 도레미 컨버터 함수 실행시키기 -> 파일경로, 파일명 사용
         p = Popen(["test.exe"], shell=True, stdout=PIPE, stdin=PIPE)
         print(p.stdout.read())
+
         # (추가)원래 파일 삭제
 
         # 변환된 파일을 찾기 -> result_파일명.jpg
+        
 
         # 이메일로 전송해주기
         send_email('안녕하세요', '내용입니다', 'domisolConverter@gmail.com',
